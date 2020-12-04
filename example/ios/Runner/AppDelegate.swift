167e425a65b3bf11ebcd07e8a72745646039e3cd
“林焕坍” <linhuantan@weeget.cn>
2020-12-04 20:47:20 +0800

    fix

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -3,23 +3,65 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
+    
+    var navigationController: UINavigationController?
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    let controller : FlutterViewController = window?.rootViewController as! FlutterViewController;
+    let controller : FlutterViewController = FlutterViewController();
+    self.navigationController = UINavigationController.init(rootViewController: controller);
+    window = UIWindow(frame: UIScreen.main.bounds);
+    window?.rootViewController = self.navigationController;
+
+    window?.makeKeyAndVisible();
+    
     let channel : FlutterMethodChannel = FlutterMethodChannel.init(name: "flutter_plugin_ly", binaryMessenger: controller.binaryMessenger);
+    
     channel.setMethodCallHandler({
         (call:FlutterMethodCall,result:FlutterResult) -> Void in
-        if("getPlatformVersion" == call.method){
-            
-            result("Android laji");
+        if("jumpPage" == call.method){
+            self.goToNativePage(result: result);
         }
-    });
+    })
     
-    
+//    let channel : FlutterMethodChannel = FlutterMethodChannel.init(name: "flutter_plugin_ly", binaryMessenger: controller.binaryMessenger);
+//    self.navigationController = UINavigationController.init(rootViewController: controller);
+//
+//
+//
+//    channel.setMethodCallHandler({
+//        (call:FlutterMethodCall,result:FlutterResult) -> Void in
+//        if("getPlatformVersion" == call.method){
+//            receiveBatteryLevel(result: result);
+//           // result("Android laji");
+//        }else if("jumpPage" == call.method){
+//            result("ok");
+//            UIApplication.shared.keyWindow?.rootViewController?.present(FirstViewController(), animated:false, completion:nil)
+//        }
+//    });
+//
+//
     
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+    
+    private func goToNativePage(result:FlutterResult) {
+        print("页面跳转");
+        let vc:UIViewController = FirstViewController()
+        vc.navigationItem.title = "新页面"
+        self.navigationController?.pushViewController(vc, animated: true);
+//        self.navigationController?.pushViewController(vc, animated: true);
+    }
+
 }
+
+
+private func receiveBatteryLevel(result: FlutterResult) {
+  let device = UIDevice.current;
+  device.isBatteryMonitoringEnabled = true;
+    result(Int(device.batteryLevel * 100));
+}
+
--- lib/flutter_plugin_ly.dart
+++ lib/flutter_plugin_ly.dart
@@ -1,4 +1,3 @@
-
 import 'dart:async';
 
 import 'package:flutter/services.dart';
@@ -7,8 +6,12 @@ class FlutterPluginLy {
   static const MethodChannel _channel =
       const MethodChannel('flutter_plugin_ly');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+  static Future<int> get platformVersion async {
+    final int version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  static Future<String> jumpPage() async {
+    return await _channel.invokeMethod("jumpPage");
   }
 }
